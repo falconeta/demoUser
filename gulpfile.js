267d422e936fab140ac5cc9a3a3330ee65fa6670
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-02-22 09:44:20 +0100

    installed gulp libraries and created dist directory

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+//jshint ignore: start
+var gulp=require('gulp');
+var gutil=require('gulp-util');
+var sourcemaps=require('gulp-sourcemaps');
+var concat=require('gulp-concat');
+var uglify=require('gulp-uglify');
+var jshint=require ('gulp-jshint');
+
+
+gulp.task('default', function(){
+    return gutil.log('Gulp è in esecuzione');
+});
+
+
+gulp.task('jshint', function(){
+    return gulp.src('weather.js').pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
