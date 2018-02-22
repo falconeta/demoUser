1b675a00578f82b38297a2e6fb96249136279afc
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-02-22 11:32:11 +0100

    riaggiunti file js cancellati per errore

---
+++ source/javascript/index.js
@@ -0,0 +1,33 @@
+/*exported paginaCaricata */
+/*global extractRandomUser*/
+function paginaCaricata() {
+    "use strict";
+    var persona = extractRandomUser('it');
+    var userId = ['email', 'born', 'name', 'gender', 'street', 'city', 'state', 'postcode', 'image'];
+    var userValue = [persona.email, persona.dob, persona.name.first + ' ' + persona.name.last,
+    persona.gender, persona.location.street, persona.location.city,
+    persona.location.state, persona.location.postcode, persona.picture.large];
+    for (var i = 0; i < userId.length; i++) {
+        carica(userId[i], userValue[i]);
+    }
+    var dob = document.getElementById('born');/*****conversione date of Born*****/
+    var born = dob.outerText;
+    dob.innerText = born.slice(8, 10) + '-' + born.slice(5, 7) + '-' + born.slice(0, 4);
+    var gender = document.getElementById('gender');
+    var genderImage = document.getElementById('imageGender');
+    switch (gender.outerText) {
+        case 'Male':
+            genderImage.src = 'https://png.icons8.com/metro/1600/male.png';
+            break;
+        case 'Female':
+            genderImage.src = 'https://png.icons8.com/metro/1600/female.png';
+            break;
+        default:}
+}
+function carica(id, value) {// funzione di caricamento
+    "use strict";
+    var name = document.getElementById(id);
+    if (id === 'image') {
+        name.src = value;
+    } else { name.innerText = value; }
+}
