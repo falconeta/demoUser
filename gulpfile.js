8fb7be14cf33d09c96b2ae7a7862c5c5b3b1fa80
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-02-22 10:27:05 +0100

    aggiunto task gulp di build e corretto task jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,33 @@
 //jshint ignore: start
-var gulp=require('gulp');
-var gutil=require('gulp-util');
-var sourcemaps=require('gulp-sourcemaps');
-var concat=require('gulp-concat');
-var uglify=require('gulp-uglify');
-var jshint=require ('gulp-jshint');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var replace = require('gulp-string-replace');
 
 
-gulp.task('default', function(){
-    return gutil.log('Gulp è in esecuzione');
+gulp.task('jshint', function () {
+    return gulp.src('source/javascript/userGenerator.js').pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish')),
+        gulp.src('source/javascript/index.js').pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
 });
 
 
-gulp.task('jshint', function(){
-    return gulp.src('userGenerator.js').pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+gulp.task('build', function () {
+    gulp.src('source/html/*.html')
+        .pipe(replace('../css/style.css', 'style.css'))
+        .pipe(replace('<script src="../javascript/userGenerator.js"></script>', ''))
+        .pipe(replace('<script src="../javascript/index.js"></script>', '<script src="bundle.js"></script>'))
+        .pipe(gulp.dest('dist'));
+    gulp.src('source/css/*.css').pipe(gulp.dest('dist'));
+    gulp.src('source/javascript/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(uglify()).pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copiaHtml',function(){
-    gulp.src('source/html/*.html').pipe(gulp.dest('dist'));
-    gulp.src('source/javascript/*.js').pipe(gulp.dest('dist'));
-});
 
-gulp.task('build-js', function(){
-    return gulp.src('source/javascript/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(concat('bundle.js'))
-    .pipe(uglify()).pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'));
-});
